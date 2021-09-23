f8a696c87765888dae65adf31e4e7c27c6042386
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-23 17:48:23 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,5 @@ apt upgrade
 apt install python 
 apt install python2
 apt install python3 
-apt install ruby
-pkg install git
-gem install lolcat
 echo " Succesful installed Ultra-DDos!" | lolcat -p 1.1
 echo " Run command: python2 ddos.py" | lolcat -p 1.1

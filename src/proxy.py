a70dd15d4498f18bdd715fdd12bb9852e9c58fce
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-23 21:50:21 +0300

    Create proxy.py

---
+++ src/proxy.py
@@ -0,0 +1,36 @@
+import requests
+import sys
+import time
+import random
+import threading
+
+on = False
+def main():
+	global pprr
+	global list
+	global proxy
+	global url
+	global pow
+	global thr
+	url = str(input("Target: " ))
+	thr = int(input("Threads: " ))
+	po = str(input("Port: " ))
+	cho = str(input("Get New Proxies? (y/n): " ))
+	if cho =='y':
+		if po =='80':
+			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1000&country=all&ssl=all&anonymity=all')
+			with open("proxies.txt","wb") as fp:
+				fp.write(rsp.content)
+				print("Sucess Download Proxies List!")
+		else:
+			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1000&country=all&ssl=yes&anonymity=all')
+			with open("proxies.txt","wb") as fp:
+				fp.write(rsp.content)
+				print("Sucess Download Https Proxies List!")
+	else:
+		pass
+	list = str(input("Proxies List (proxies.txt): " ))
+	pprr = open(list).readlines()
+	print("Proxies Count: "  + "%d" %len(pprr))
+	pow = int(input("Multiplier (1-100):" ))
+	opth()

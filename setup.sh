7fb0b24c4aec7e1e292c7ef22ce933fa3b6af2b0
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-09-24 19:43:44 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-clear
-apt update
-apt upgrade
-apt install python 
-apt install python2
-apt install python3 
-echo " Succesful installed Ultra-DDos!" | lolcat -p 1.1
-echo " Run command: python2 ddos.py" | lolcat -p 1.1

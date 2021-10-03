0d74cb114641133c0383cf331b4b8de7b014ee05
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-03 10:31:31 +0300

    Update SiteIp.py

--- SiteIp.py
+++ SiteIp.py
@@ -2,10 +2,15 @@
 import socket
 import os
 
+red='\033[31m'
+green='\033[32m'
+yellow = '\033[93m'
+
 target = input(f"{green}Enter Target URL: ")
 target.replace("http://", "")
 target.replace("https://","")
 target.replace("www.","")
+
 ip = socket.gethostbyname(target)
 port = 8020 # recommend ports: 8020, 8080, 4040, 1228, 6699
 joker = "DDOSjsjsjjdjdjdjdjjjjjjjjjiiiiiiiopppkkkkjjjjjhhhbbbbgbvvvvvvvvvvvvhhyggggh"

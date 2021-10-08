142a50da98247443d6c916577824914c36914a8c
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-10-08 16:34:57 +0300

    Update log-ddos.py

--- src/log-ddos.py
+++ src/log-ddos.py
@@ -12,7 +12,7 @@ C = '\033[36m'
 W = '\033[0m'
 
 def ddos():
-	os.system('python src/logo.py')
+	os.system('python logo.py')
 	print('')
 	print(G + "Terminate attack by clicking: CTRL + Z")
 	users = [
